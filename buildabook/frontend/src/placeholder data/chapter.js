fa9833d1bf893f9d4307c76674c2682dcf02e68c
johnc <aasolidgaming@gmail.com>
2020-03-30 20:37:35 -0400

    Added contenders array to chapters and test data to reflect it

--- buildabook/frontend/src/placeholder data/chapter.js
+++ buildabook/frontend/src/placeholder data/chapter.js
@@ -1,15 +1,49 @@
 var chapterData = [
     {
         "_id": 1,
-        "title": "Chapter 1",
+        "title": "Chapter 1a",
         "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed id semper risus in hendrerit gravida rutrum quisque. Urna et pharetra pharetra massa. Commodo quis imperdiet massa tincidunt. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. A lacus vestibulum sed arcu non. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a. Pulvinar neque laoreet suspendisse interdum. Sit amet consectetur adipiscing elit. Eget nunc scelerisque viverra mauris in. Condimentum mattis pellentesque id nibh tortor. Condimentum mattis pellentesque id nibh tortor id aliquet. Gravida arcu ac tortor dignissim. Consectetur purus ut faucibus pulvinar elementum integer enim neque. Facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris. Turpis massa tincidunt dui ut ornare lectus sit amet est.",
         "author": 'David',
         "experation": "",
         "comments": [],
         "upvoteCount": 2,
-        "dateCreated": ""
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": [11, 12]
 
     },
+
+    {
+        "_id": 11,
+        "title": "Chapter 1a",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed id semper risus in hendrerit gravida rutrum quisque. Urna et pharetra pharetra massa. Commodo quis imperdiet massa tincidunt. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. A lacus vestibulum sed arcu non. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum a. Pulvinar neque laoreet suspendisse interdum. Sit amet consectetur adipiscing elit. Eget nunc scelerisque viverra mauris in. Condimentum mattis pellentesque id nibh tortor. Condimentum mattis pellentesque id nibh tortor id aliquet. Gravida arcu ac tortor dignissim. Consectetur purus ut faucibus pulvinar elementum integer enim neque. Facilisi nullam vehicula ipsum a arcu cursus vitae congue mauris. Turpis massa tincidunt dui ut ornare lectus sit amet est.",
+        "author": 'David',
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 2,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    {
+        "_id": 12,
+        "title": "Chapter 1b",
+        "text": "Kill Roy was here",
+        "author": 'John',
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 1,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
     {
         "_id": 2,
         "title": "Chapter 2",
@@ -18,9 +52,48 @@ var chapterData = [
         "experation": "",
         "comments": [],
         "upvoteCount": 300,
-        "dateCreated": ""
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": [21, 22]
 
     },
+
+    
+    {
+        "_id": 21,
+        "title": "Chapter 2a",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed ullamcorper morbi tincidunt ornare. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna. Donec et odio pellentesque diam volutpat commodo sed. Sit amet nulla facilisi morbi tempus iaculis urna id. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Mi bibendum neque egestas congue quisque. Pharetra vel turpis nunc eget lorem. Ac turpis egestas integer eget aliquet nibh. Facilisis magna etiam tempor orci. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Aenean pharetra magna ac placerat vestibulum. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Quam viverra orci sagittis eu volutpat.",
+        "author": "Blake",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 300,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    
+    {
+        "_id": 22,
+        "title": "Chapter 2b",
+        "text": "And quote the Raven: \"Error 404\"",
+        "author": "John",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 30,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    
+
+
     {
         "_id": 3,
         "title": "",
@@ -29,9 +102,45 @@ var chapterData = [
         "experation": "",
         "comments": [],
         "upvoteCount": 3,
-        "dateCreated": ""
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": [31, 32]
 
     },
+
+    {
+        "_id": 31,
+        "title": "",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Nec sagittis aliquam malesuada bibendum arcu. Pellentesque habitant morbi tristique senectus. Felis imperdiet proin fermentum leo. Scelerisque fermentum dui faucibus in ornare. Nibh tellus molestie nunc non blandit massa enim nec dui. Pellentesque pulvinar pellentesque habitant morbi tristique. Phasellus vestibulum lorem sed risus ultricies. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut. Mattis pellentesque id nibh tortor id aliquet lectus. Eget gravida cum sociis natoque penatibus. Cras sed felis eget velit aliquet. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Aliquet nibh praesent tristique magna sit. Tellus id interdum velit laoreet. Potenti nullam ac tortor vitae purus faucibus. Tellus elementum sagittis vitae et leo duis ut diam quam.",
+        "author": "John",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 3,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    {
+        "_id": 32,
+        "title": "",
+        "text": "This is not the way",
+        "author": "Bobba fett lookin dude",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 2,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    
+
     {
         "_id": 4,
         "title": "",
@@ -40,9 +149,43 @@ var chapterData = [
         "experation": "",
         "comments": [],
         "upvoteCount": 3,
-        "dateCreated": ""
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": [41, 42]
 
     },
+
+    {
+        "_id": 41,
+        "title": "",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae tortor condimentum lacinia quis vel eros donec ac. Elit duis tristique sollicitudin nibh. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Diam sollicitudin tempor id eu. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Adipiscing elit ut aliquam purus sit. Odio ut sem nulla pharetra. Egestas tellus rutrum tellus pellentesque eu tincidunt. Quis vel eros donec ac odio tempor. Porta nibh venenatis cras sed felis eget velit aliquet. Turpis egestas pretium aenean pharetra magna. Porttitor lacus luctus accumsan tortor posuere ac ut consequat semper. Et malesuada fames ac turpis. Lacinia at quis risus sed vulputate odio ut. Sit amet nisl purus in mollis nunc sed id semper. Vitae purus faucibus ornare suspendisse sed. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Mattis rhoncus urna neque viverra justo nec ultrices.",
+        "author": "Joe",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 3,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    {
+        "_id": 42,
+        "title": "",
+        "text": "HMmmmmmmmmmmmmmmmmMMMMMMMMMMMMMMMmmmmmmmmmmmMMMMMMmm.",
+        "author": "TheThinker",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 1,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
     {
         "_id": 5,
         "title": "",
@@ -51,9 +194,43 @@ var chapterData = [
         "experation": "",
         "comments": [],
         "upvoteCount": 3,
-        "dateCreated": ""
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": [51, 52]
 
     },
+
+    {
+        "_id": 51,
+        "title": "",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis cras sed felis eget velit aliquet sagittis id consectetur. Sed cras ornare arcu dui vivamus. Tellus rutrum tellus pellentesque eu. Quis risus sed vulputate odio. Posuere ac ut consequat semper viverra nam libero. Et ligula ullamcorper malesuada proin libero nunc consequat. Consequat interdum varius sit amet mattis vulputate enim nulla aliquet. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero. Tincidunt eget nullam non nisi est sit amet. At elementum eu facilisis sed odio morbi quis commodo. Amet luctus venenatis lectus magna fringilla urna porttitor. Nunc congue nisi vitae suscipit tellus. Lectus proin nibh nisl condimentum id venenatis a. Semper risus in hendrerit gravida. Mattis rhoncus urna neque viverra justo nec. Nisl pretium fusce id velit ut. Sit amet consectetur adipiscing elit ut aliquam. Nunc aliquet bibendum enim facilisis gravida neque.",
+        "author": "Elias",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 80,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    {
+        "_id": 52,
+        "title": "RIP AND TEAR",
+        "text": "RIP AND TEAR RIP AND TEAR RIP AND TEAR",
+        "author": "DoomGuy",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 90,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
     {
         "_id": 6,
         "title": "",
@@ -62,7 +239,40 @@ var chapterData = [
         "experation": "",
         "comments": [],
         "upvoteCount": 3,
-        "dateCreated": ""
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": [61, 62]
+
+    },
+
+    {
+        "_id": 61,
+        "title": "",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At imperdiet dui accumsan sit amet nulla. At quis risus sed vulputate. Nunc sed id semper risus. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Erat pellentesque adipiscing commodo elit at. Potenti nullam ac tortor vitae purus faucibus ornare. Proin sed libero enim sed. Massa tempor nec feugiat nisl pretium fusce. Quam nulla porttitor massa id neque aliquam vestibulum. Tincidunt eget nullam non nisi est sit amet. Nisl purus in mollis nunc sed id. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+        "author": "Alex",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 3,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
+
+    },
+
+    {
+        "_id": 62,
+        "title": "",
+        "text": "Why do they call a lisp a lisp if its hard for people with lisps to say it?",
+        "author": "John",
+        "experation": "",
+        "comments": [],
+        "upvoteCount": 3,
+        "dateCreated": "",
+        "startDate":"",
+        "endDate":"",
+        "contenders": []
 
     }
 
